426abd4e9d33aec227e785c44a653dc1813be5f8
wangxi <wangxi8206@sina.com>
2021-01-30 17:53:25 +0800

    add swap page

--- src/components/Wallet/Swap.vue
+++ src/components/Wallet/Swap.vue
@@ -1,19 +1,42 @@
 <template>
-    <div>
-        swap
+    <div class="wallet-swap">
+      <div class="tab">
+        <div :class="['tab-card', currentTab === 'tsteem' ? 'checked' : '']" @click="change('tsteem')">
+          <img src="../../static/images/tsteem.svg" alt="">
+          <h5>TSTEEM</h5>
+        </div>
+        <div :class="['tab-card', currentTab === 'tsbd' ? 'checked' : '']" @click="change('tsbd')">
+          <img src="../../static/images/tsbd.svg" alt="">
+          <h5>TSBD</h5>
+        </div>
+      </div>
+      <SwapField :tokenType="currentTab"/>
     </div>
 </template>
 
 <script>
+import Card from '../ToolsComponents/Card'
+import SwapField from './SwapField'
 export default {
   name: 'Swap',
   data () {
     return {
+      currentTab: 'tsteem',
 
     }
   },
+  components: {
+    Card,
+    SwapField
+  },
   methods: {
-
+    change (tab){
+      if (tab === 'tsteem'){
+        this.currentTab = 'tsteem'
+      }else if (tab === 'tsbd'){
+        this.currentTab = 'tsbd'
+      }
+    }
   },
   mounted () {
     ;
@@ -22,5 +45,41 @@ export default {
 </script>
 
 <style lang="less" scoped>
-
+.wallet-swap {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  flex-wrap: wrap;
+  .tab{
+    width: 100%;
+    display: flex;
+    align-items: left;
+    .tab-card{
+      margin:20px 14px;
+      border-radius: 8px;
+      border: 2px dotted var(--yellow-background);
+      padding: 10px 16px;
+      background-color: white;
+      display: flex;
+      align-items: center;
+      width:160px;
+      box-sizing: border-box;
+      h5{
+        margin-bottom:0;
+        margin-left: 10px;
+      }
+      img{
+        width: 30px;
+        height: 30px;
+      }
+    }
+    .tab-card:hover{
+      cursor: pointer;
+      background-color: var(--yellow-background);
+    }
+    .checked{
+      border: 1px solid var(--primary);
+    }
+  }
+}
 </style>
---
+++ src/components/Wallet/SwapField.vue
@@ -0,0 +1,81 @@
+<template>
+    <div class="swap-field">
+        <Card>
+            <div class="box">
+                <div class="round-box">
+                    <div class="box-title-container">
+                        <span>from</span>
+                        <span>{{$t('message.balance') + ': ' + steemBalance +' STEEM'}}</span>
+                    </div>
+                    <div class="box-content-container">
+
+                    </div>
+                </div>
+            </div>
+        </Card>
+    </div>
+</template>
+
+<script>
+import Card from '../ToolsComponents/Card'
+import { mapState } from 'vuex'
+
+export default {
+    name: 'SwapField',
+    components: {
+        Card,
+    },
+    computed: {
+        ...mapState(['steemBalance'])
+    },
+    props: {
+        tokenType: {
+            type: String,
+        },
+    },
+    filters: {
+        uppercase: function(value) {
+            return value.toUpperCase();
+        }
+    },
+    methods: {
+        checkInput() {
+            
+        }
+    },
+    mounted () {
+        ;
+    },
+}
+</script>
+
+<style lang="less" scoped>
+.swap-field {
+    margin-top:48px;
+    .box {
+        width:400px;
+        padding-top: 30px;
+        h4{
+            margin-bottom: 20px;
+        }
+        .round-box {
+           border-radius: 20px;
+           border: 1px solid rgb(247, 248, 250);
+           padding: 8px 14px;
+           margin-bottom: 1em;
+        }
+        .box-title-container{
+            display: flex;
+            justify-content: space-between;
+            span{
+                font-weight: 500;
+                font-size:14px;
+            }
+        }
+        .box-content-container{
+            display: flex;
+
+        }
+    }
+}
+</style>
