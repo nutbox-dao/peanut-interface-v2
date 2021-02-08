c1acdc4f5611640a6934757e575ca3c3d9fe3aff
wangxi <wangxi8206@sina.com>
2021-02-08 23:34:41 +0800

    fix

--- vue.config.js
+++ vue.config.js
@@ -13,17 +13,6 @@ module.exports = {
       }
     }
   },
-  proxy: {
-    '/nps': { 
-      // target: 'http://1.15.101.110:3000/nps',
-      target: 'http://127.0.0.1:3000/nps',
-      changeOrigin: true,
-      ws: true,
-      pathRewrite: {
-        '^/nps': ''
-      }
-    }
-  },
   pluginOptions: {
     i18n: {
       locale: 'en',
