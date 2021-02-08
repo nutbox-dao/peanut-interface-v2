50e392f74dc7a150e0d0be233fe616f58c24b71e
wangxi <wangxi8206@sina.com>
2021-02-08 23:39:30 +0800

    1

--- src/apis/api.js
+++ src/apis/api.js
@@ -1,3 +1,3 @@
 import { get } from './axios'
 
-export const getProposal = async (params) => get('http://127.0.0.1:3000/nps/getProposal', params)
+export const getProposal = async (params) => get('http://1.15.101.110:3000/nps/getProposal', params)
