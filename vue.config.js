5b5caa67579a21661e6f6696447b65fcb1c6bbab
wangxi <wangxi8206@sina.com>
2021-02-08 15:16:21 +0800

    update api host

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,8 @@ module.exports = {
   devServer: {
     proxy: {
       '/nps': { 
-        target: 'http://1.15.101.110:3000/nps',
+        // target: 'http://1.15.101.110:3000/nps',
+        target: 'http://127.0.0.1:3000/nps',
         changeOrigin: true,
         ws: true,
         pathRewrite: {
