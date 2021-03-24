9ec53e5933109da7b0d0a037d32ee262ffcebb7b
wangxi <wangxi8206@sina.com>
2021-03-24 17:46:57 +0800

    update tsteem pool address

--- src/config.js
+++ src/config.js
@@ -59,7 +59,7 @@ export const PNUT_LP_POOL_ADDRESS = 'TLud51bQbsvA3fGPoXw5e9NPb117ckYn5y'
 
 // TSTEEM
 export const TSTEEM_TOKEN_ADDRESS = 'TDZFuwYAnHmMkrZUvoEVBSLfLQv66fx3YJ'
-export const TSTEEM_POOL_ADDRESS = 'TJMpvYyT2dm8NNgAAd3v6mLEZSgzBLfcqY'
+export const TSTEEM_POOL_ADDRESS = 'TFccHHX5eJ6zcLa7ToYAZbn4NdAzDgFfJW'
 
 // approvement
 export const APPROVE_TOKEN_CONTRACT_PAIR = {
