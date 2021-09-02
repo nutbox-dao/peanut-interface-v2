7c9296731bc55740b5b1674644a2d226cff14ec5
HaleShaw@163.com <HaleShaw@163.com>
2021-09-02 08:15:47 +0800

    Format code

--- src/assets/lang/zh_CN.js
+++ src/assets/lang/zh_CN.js
@@ -7,18 +7,18 @@ module.exports = {
     delegatemine: '代理挖矿',
     spbalance: 'SP余额',
     yourspdelegate: 'SP代理量',
-    cancel:'取消',
-    confirm:'确认',
+    cancel: '取消',
+    confirm: '确认',
 
     sptotaldelegate: '代理总量',
     totalpnut: 'PNUT总产量',
     apy: '年化收益率',
     pnutprofits: 'PNUT收益',
     withdraw: '收获',
-    withdrawn:"已收获",
+    withdrawn: '已收获',
     balance: '余额',
-    approveContract:"授权合约",
-    max:'最大',
+    approveContract: '授权合约',
+    max: '最大',
     confirmconvert: '确认兑换',
     servicecharge: '手续费',
     delegatecharge: '代理手续费',
@@ -34,10 +34,10 @@ module.exports = {
     tsbdbalance: 'TSBD余额',
     pnutbalance: 'PNUT余额',
     input: '输入',
-    tips:"帮助信息",
+    tips: '帮助信息',
 
     setting: '设置',
-    admin:'管理员',
+    admin: '管理员',
     whitePaper: '白皮书',
     nps: 'Nutbox提案系统',
     userBook: '用户手册',
@@ -65,22 +65,22 @@ module.exports = {
   wallet: {
     wallet: '钱包',
     swap: '跨链转账',
-    connectSteem:"链接Steem钱包",
-    connectTron:"链接波场钱包",
-    swapSubTitle:"及时、安全的将资产转移到波场网络",
-    install:"立即安装",
+    connectSteem: '链接Steem钱包',
+    connectTron: '链接波场钱包',
+    swapSubTitle: '及时、安全的将资产转移到波场网络',
+    install: '立即安装'
   },
   stake: {
     stake: '质押挖矿',
-    creaseDelegation: "增加代理",
-    increaseDelegation: "减少代理",
-    getSp: "获取 SP",
-    get:'获取',
+    creaseDelegation: '增加代理',
+    increaseDelegation: '减少代理',
+    getSp: '获取 SP',
+    get: '获取'
   },
   farm: {
     farm: '流动性挖矿',
-    stake:"储蓄",
-    unStake: "减少储蓄",
+    stake: '储蓄',
+    unStake: '减少储蓄',
     tsp: {
       tspMine: 'TSP挖矿',
       yourTspAmount: '储蓄的TSP数量',
@@ -90,7 +90,7 @@ module.exports = {
       confirmDeposit: '确认储蓄',
       addTspDeposit: '增加储蓄',
       minusTspDeposit: '减少储蓄',
-      notEnoughFee: '手续费不足',
+      notEnoughFee: '手续费不足'
     },
     tsteem: {
       tsteemMine: 'TSTEEM挖矿',
@@ -101,44 +101,44 @@ module.exports = {
       confirmDeposit: '确认储蓄',
       addTsteemDeposit: '增加储蓄',
       minusTsteemDeposit: '减少储蓄',
-      notEnoughFee: '手续费不足',
+      notEnoughFee: '手续费不足'
     },
     tspLp: {
       tspLPBalance: 'S-TSP-TRX 余额',
       yourTSPLPAmount: '储蓄的S-TSP-TRX',
-      totalDepositTspLP:"储蓄总量",
-      totalTSP: 'TSP总量',
+      totalDepositTspLP: '储蓄总量',
+      totalTSP: 'TSP总量'
     },
     pnutLp: {
       pnutLPBalance: 'S-PNUT-TRX 余额',
       yourPNUTLPAmount: '储蓄的S-PNUT-TRX',
-      totalDepositPnutLP:'储蓄总量'
+      totalDepositPnutLP: '储蓄总量'
     }
   },
   liquidStaking: {
     liquidStaking: '质押资产流动',
-    title: "TSP 质押与赎回",
+    title: 'TSP 质押与赎回',
     tsp: {
       tspIsCirculatingSP: 'TSP为SP的等价物，TSP可享受SP同等的质押奖励，且TSP可交易',
       tsptosteemintro: '转换的STEEM将在33-40天内陆续返还'
     }
   },
-  vote:{
-    upvote:"点赞",
-    vote:"点赞帖子",
-    title:"支付PNUT,获得nutbox.mine点赞",
-    link:"帖子链接",
-    payPnut:"支付PNUT",
-    linkPlaceholder:"请填写需要点赞的帖子链接",
-    pnutPlaceholder:"请填写支付金额",
-    transfer:"转账",
-    voteRate:"PNUT - 10% 点赞",
+  vote: {
+    upvote: '点赞',
+    vote: '点赞帖子',
+    title: '支付PNUT,获得nutbox.mine点赞',
+    link: '帖子链接',
+    payPnut: '支付PNUT',
+    linkPlaceholder: '请填写需要点赞的帖子链接',
+    pnutPlaceholder: '请填写支付金额',
+    transfer: '转账',
+    voteRate: 'PNUT - 10% 点赞'
   },
-  airdrop:{
+  airdrop: {
     airdrop: '空投',
-    comingSoon:"敬请期待",
-    whereinDes:"对WhereIN & CNSTM令牌持有者空投",
-    nutboxDes:"PNUT世纪大空投"
+    comingSoon: '敬请期待',
+    whereinDes: '对WhereIN & CNSTM令牌持有者空投',
+    nutboxDes: 'PNUT世纪大空投'
   },
   nps: {
     nps: '提案改善',
@@ -170,13 +170,13 @@ module.exports = {
     illegalTronAddress: '无效的波场地址',
     inputError: '输入值无效',
     inputOverflow: '输入超过最大值',
-    inputOutOfRange: "请输入{lowerPnutAmount}-{upperPnutAmount}的值",
-    inputLinkIllegal:"请输入正确的链接",
-    transferFail:"转账失败",
+    inputOutOfRange: '请输入{lowerPnutAmount}-{upperPnutAmount}的值',
+    inputLinkIllegal: '请输入正确的链接',
+    transferFail: '转账失败',
     notEnoughFee: '手续费不足',
-    insufficentPnut:"PNUT余额不足",
-    hasVoted: "Nutbox已经点赞了您的帖子。",
-    pleaseRetry: "请重试"
+    insufficentPnut: 'PNUT余额不足',
+    hasVoted: 'Nutbox已经点赞了您的帖子。',
+    pleaseRetry: '请重试'
   }
 
 }
