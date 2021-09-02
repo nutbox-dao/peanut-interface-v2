340f0cf7a3920a922be324a8c27ed461d9e8c10a
HaleShaw@163.com <HaleShaw@163.com>
2021-09-02 18:42:48 +0800

    Format code

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -4,19 +4,20 @@
       <div class="login">
         <p>{{ this.$t("message.userlogin") }}</p>
         <div class="account-box">
-          <span class="keychain" @click="getKeychain"/>
+          <span class="keychain" @click="getKeychain" />
           <b-input
             class="mb-2 mr-sm-2 mb-sm-0 input"
             :placeholder="$t('message.steemAccoutPlaceHolder')"
             v-model="steemAccount"
           ></b-input>
         </div>
-        <b-button variant="primary" class="login-btn" @click="login" :disabled="isLoging">
-          <b-spinner
-            small
-            type="grow"
-            v-show="isLoging"
-          ></b-spinner>
+        <b-button
+          variant="primary"
+          class="login-btn"
+          @click="login"
+          :disabled="isLoging"
+        >
+          <b-spinner small type="grow" v-show="isLoging"></b-spinner>
           {{ this.loginBtnText }}
         </b-button>
       </div>
@@ -32,79 +33,82 @@
 </template>
 
 <script>
-import TipMessage from "./ToolsComponents/TipMessage";
+import TipMessage from './ToolsComponents/TipMessage'
 
 export default {
-  name: "Login",
-  data() {
+  name: 'Login',
+  data () {
     return {
-      loginBtnText: "",
+      loginBtnText: '',
       isLoging: false,
-      steemAccount: "",
-      tipTitle: "",
-      tipMessage: "",
+      steemAccount: '',
+      tipTitle: '',
+      tipMessage: '',
       showMessage: false,
-      canDismissTip: true,
-    };
+      canDismissTip: true
+    }
   },
   components: {
-    TipMessage,
+    TipMessage
   },
   methods: {
-    getKeychain() {
-      window.open('https://chrome.google.com/webstore/detail/steem-keychain/lkcjlnjfpbikmcmbachjpdbijejflpcm','_blank')
+    getKeychain () {
+      window.open(
+        'https://chrome.google.com/webstore/detail/steem-keychain/lkcjlnjfpbikmcmbachjpdbijejflpcm',
+        '_blank'
+      )
     },
-    login() {
-      if (this.steemAccount.length === 0){
-        return;
+    login () {
+      if (this.steemAccount.length === 0) {
+        return
       }
       const message = `nutbox_login-${Math.floor(
         100000000 + Math.random() * 900000000
-      )}`;
-      this.isLoging = true;
-      const that = this;
+      )}`
+      this.isLoging = true
+      const that = this
       steem_keychain.requestSignBuffer(
         this.steemAccount,
         message,
-        "Active",
+        'Active',
         async function (res) {
           if (res.success === true) {
             const ress = await that.$store.dispatch(
-              "initializeSteemAccount",
+              'initializeSteemAccount',
               res.data.username
-            );
+            )
             if (!ress) {
-              that.tipTitle = that.$t("error.error");
-              that.tipMessage = that.$t("error.steemLoginFail");
-              that.showMessage = true;
-              that.isLoging = false;
-              return;
+              that.tipTitle = that.$t('error.error')
+              that.tipMessage = that.$t('error.steemLoginFail')
+              that.showMessage = true
+              that.isLoging = false
+              return
             }
-            that.$emit("hideMask");
+            that.$emit('hideMask')
           } else {
-            if (res.error === "user_cancel") {
-              that.tipTitle = that.$t("error.error");
-              that.tipMessage = that.$t("error.unlockKeychain");
-              that.showMessage = true;
+            if (res.error === 'user_cancel') {
+              that.tipTitle = that.$t('error.error')
+              that.tipMessage = that.$t('error.unlockKeychain')
+              that.showMessage = true
             } else {
-              that.tipTitle = that.$t("error.error");
-              that.tipMessage = that.$t("error.steemLoginFail");
-              that.showMessage = true;
+              that.tipTitle = that.$t('error.error')
+              that.tipMessage = that.$t('error.steemLoginFail')
+              that.showMessage = true
             }
           }
-          that.isLoging = false;
+          that.isLoging = false
         }
-      );
+      )
     },
-    hideMask() {
-      if (this.isLoging) return;
-      this.$emit("hideMask");
-    },
+    hideMask () {
+      if (this.isLoging) return
+      this.$emit('hideMask')
+    }
   },
-  async mounted() {
-    this.loginBtnText = this.$t("message.login");
-  },
-};
+  async mounted () {
+    this.loginBtnText = this.$t('message.login')
+  }
+}
 </script>
 
 <style lang="less" scoped>
@@ -116,36 +120,37 @@ export default {
   box-shadow: 0px 2px 6px 0px rgba(0, 0, 0, 0.05);
   border-radius: 28px;
   padding: 24px;
-  p{
-  font-size: 20px;
+  p {
+    font-size: 20px;
   }
 }
 .account-box {
   display: flex;
   margin-top: 40px;
-.keychain{
-  width: 180px;
-  height: 48px;
-  background-color: var(--background);
-  margin-right: 2px;
-  border-radius:16px 0 0 16px;
-  background-image: url('../static/images/keychain.png');
-  background-repeat: no-repeat;
-  background-position: center;
+  .keychain {
+    width: 180px;
+    height: 48px;
+    background-color: var(--background);
+    margin-right: 2px;
+    border-radius: 16px 0 0 16px;
+    background-image: url("../static/images/keychain.png");
+    background-repeat: no-repeat;
+    background-position: center;
+  }
+  .keychain:hover {
+    background-color: var(--dividers);
+    cursor: pointer;
+  }
+  .input {
+    height: 48px;
+    flex: 1;
+    background: var(--background);
+    border-radius: 0 16px 16px 0;
+    border: none;
+  }
 }
-.keychain:hover {
-  background-color:var(--dividers);
-  cursor: pointer;
-}
-.input {
-  height: 48px;
-  flex: 1;
-  background: var(--background);
-  border-radius: 0 16px 16px 0;
-  border: none;
-}
-}
-.login-btn{
-  width: 100%;margin-top: 24px;
+.login-btn {
+  width: 100%;
+  margin-top: 24px;
 }
 </style>
--- src/utils/helper.js
+++ src/utils/helper.js
@@ -3,8 +3,7 @@ import {
   getApys,
   getPnutLpExchangeInfo,
   getTspLpExchangeInfo
-} from "../apis/api"
-
+} from '../apis/api'
 
 export const firstToUpper = function (str) {
   if (!str) {
@@ -46,9 +45,9 @@ export const retryMethod = async function (func, retries = 5, interval = 1) {
 export const formatBalance = function (value, digit = 3) {
   if (!value) return '0'
   const str =
-    digit != null && digit >= 0 ?
-    Number(value).toFixed(digit).toString() :
-    value.toString()
+    digit != null && digit >= 0
+      ? Number(value).toFixed(digit).toString()
+      : value.toString()
   let integer = str
   let fraction = ''
   if (str.includes('.')) {
@@ -58,7 +57,7 @@ export const formatBalance = function (value, digit = 3) {
   return integer.replace(/\B(?=(\d{3})+(?!\d))/g, ',') + fraction
 }
 
-export function getDateString(now, timezone, extra = 0) {
+export function getDateString (now, timezone, extra = 0) {
   now = now || new Date()
   const offset = timezone != null ? timezone * 60 : 0
   now = new Date(now.getTime() + (offset + extra) * 60 * 1000)
@@ -75,12 +74,12 @@ export const storeApy = async function () {
   const pnutLpExchangePoolApy = parseFloat(volume24H) * 365 * 0.3 / parseFloat(totalLiquidity)
   // calculate tsp-trx exchange pool apy
   const data = tspLpInfo.data
-  const tspVolume24 = data.volume24H;
-  const tspTotalLiquidity = data.totalLiquidity;
+  const tspVolume24 = data.volume24H
+  const tspTotalLiquidity = data.totalLiquidity
   const tspLpExchengePoolApy = parseFloat(tspVolume24) * 365 * 0.3 / parseFloat(tspTotalLiquidity)
   // save to cache
-  store.commit('saveApy', parseFloat(apys.spApy).toFixed(1) + "%")
-  store.commit('saveTspLpApy', parseFloat(apys.spApy).toFixed(1) + "% + " + tspLpExchengePoolApy.toFixed(1) + "%")
-  store.commit('savePnutLpApy', parseFloat(apys.pnutLpApy).toFixed(1) + "% + " + pnutLpExchangePoolApy.toFixed(1) + "%")
-  store.commit('saveTsteemApy',parseFloat(apys.tsteemApy).toFixed(1) + "%")
+  store.commit('saveApy', parseFloat(apys.spApy).toFixed(1) + '%')
+  store.commit('saveTspLpApy', parseFloat(apys.spApy).toFixed(1) + '% + ' + tspLpExchengePoolApy.toFixed(1) + '%')
+  store.commit('savePnutLpApy', parseFloat(apys.pnutLpApy).toFixed(1) + '% + ' + pnutLpExchangePoolApy.toFixed(1) + '%')
+  store.commit('saveTsteemApy', parseFloat(apys.tsteemApy).toFixed(1) + '%')
 }
